f8c714835b7333adb3a8e0c0ed5e7325627ec26f
Gnana Prakash M <gnanaprakash2918@gmail.com>
2024-05-01 18:53:52 +0530

    Add Contact Page

---
+++ src/contact.js
@@ -0,0 +1,27 @@
+function createContact() {
+  const contact = document.createElement('div');
+  contact.classList.add('contact');
+
+  const phNum = document.createElement('p');
+  phNum.textContent = '850 856 987';
+
+  const address = document.createElement('p');
+  address.textContent = 'BellWood, USA';
+
+  const restaurantLocation = document.createElement('img');
+  restaurantLocation.src = 'Images/Restaurant-Img.jpg';
+
+  contact.appendChild(phNum);
+  contact.appendChild(address);
+  contact.appendChild(restaurantLocation);
+
+  return contact;
+}
+
+function loadContact() {
+  const main = document.getElementById('content');
+  main.textContent = '';
+  main.appendChild(createContact());
+}
+
+export default loadContact;
